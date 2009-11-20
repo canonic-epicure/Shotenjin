25dca5fe3f9b0710807ea6a6edb6cd2c899ddbc8
SamuraiJack <nickolay8@gmail.com>
2009-11-20 23:19:15 +0300

    - some progress

--- t/020_parsing.t.js
+++ t/020_parsing.t.js
@@ -19,7 +19,24 @@ StartTest(function(t) {
         
         t.ok(tenjin, "'Shotenjin.Joosed' was succesfully instantiated")
         
+
+        //======================================================================================================================================================================================================================================================
+        t.diag('Parsing - splitAndProcess')
         
+        var res = []
+        
+        tenjin.splitAndProcess(res, /tag((?:.|\n)+)tag/g, '00000\n0000tag1111\n1111tag\n0000tag1111tag\n', 
+            
+            function (res, whitespace) { 
+                res.push('{' + whitespace + '}')
+            },
+            
+            function (res, match) { 
+                res.push('[' + match[1] + ']')
+            }
+        )
+        
+        t.ok(res[0] == '{00000\n0000}', 'Whitespace #1 was processed correctly')
         
         
         t.endAsync(async0)
