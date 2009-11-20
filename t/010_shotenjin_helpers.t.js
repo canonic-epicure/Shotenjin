fa8297e35c784690fb9763e55381e1aaecfeb147
SamuraiJack <nickolay8@gmail.com>
2009-11-20 17:12:53 +0300

    - some progress

--- t/010_shotenjin_helpers.t.js
+++ t/010_shotenjin_helpers.t.js
@@ -1,6 +1,6 @@
 StartTest(function(t) {
     
-	t.plan(1)
+	t.plan(3)
     
     var async0 = t.beginAsync()
     
@@ -10,6 +10,12 @@ StartTest(function(t) {
         t.diag('Sanity')
         
         t.ok(Shotenjin.Joosed.my, "Shotenjin.Joosed.my is here")
+        
+        var tenjin = Shotenjin.Joosed.my
+        
+        t.ok(tenjin.escapeXml('<xml><"more"></xml>') == '&lt;xml&gt;&lt;&quot;more&quot;&gt;&lt;/xml&gt;', 'XML escaping works')
+        
+        t.ok(tenjin.trim('  string   ') == 'string', 'Trimming works')
         
         
         t.endAsync(async0)
---
+++ t/090_draft.tj.js
@@ -0,0 +1,45 @@
+Class('My.Template', {
+    
+    meta : 'Shotenjin.Joosed',
+    
+    template : {
+        /*tj
+            Hello #{name}!
+            <ul>
+            <?js for (var i = 0, n = items.length; i < n; i++) { ?>
+             <li>${items[i]}</li>
+            <?js } ?>
+            </ul>
+        
+        
+            <div class="thumbnail photoThumbnailMid medium12">
+                
+                <p class="centered ">[% name %]</p>
+                
+                <div class="x-rawbackground paper1 photo">
+                    ${ App.my('/Travel::MainLayout.center/Travel::Photo::Single',{ 'photo_id' = current_photo.id } ) } <img width=170 height=170 src="[% src(current_photo.path, 170, 170, 'place') %]"></a>
+                    
+                    <?= this.customFunc({ 'photo_id' = current_photo.id }) ?>.my('/Travel::MainLayout.center/Travel::Photo::Single', ) }
+                </div>
+                \
+                <div class="righted">
+                    ${ I18n.words.added_by }: [% a('/Travel::MainLayout.center/Travel::User::Single',{ 'user_id' = current_photo.creator_id } ) %]<span class="lightgray2">[% current_photo.creator %]</span></a><br>
+                </div>
+            </div>
+        tj*/
+        /* CREATED BY SHOTENJIN.JOOSED HELPER SCRIPT DO NOT MODIFY DIRECTLY */
+        source : 'Hello #{name}\n...'
+        /* CREATED BY SHOTENJIN.JOOSED HELPER SCRIPT DO NOT MODIFY DIRECTLY */        
+    },
+
+    
+    methods : {
+        
+        customFunc : function (p1, p2) {
+            return format(p1, p2, this.stash.p3)
+        }
+    }
+
+})
+
+
